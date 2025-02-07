6936c91403a54d03ab3bd330c325309224c3438a
Wil Hunt <huntwj@gmail.com>
2025-02-07 19:58:34 +0000

    Fix Jest configuration

--- src/app/model/play-world.test.ts
+++ src/app/model/play-world.test.ts
@@ -1,9 +1,22 @@
 import { scoreUserGuess } from "./play-wordle";
 
+const correct = (letter: string) => ({
+  letter,
+  score: "correct",
+});
+
 describe("Play Wordle Model", () => {
   describe("Guess Checker", () => {
     it("should work with correct guesses", () => {
-      expect(scoreUserGuess("ANVIL")("ANVIL")).toEqual([]);
+      expect(scoreUserGuess("ANVIL")("ANVIL")).toEqual({
+        scoredLetters: [
+          correct("A"),
+          correct("N"),
+          correct("V"),
+          correct("I"),
+          correct("L"),
+        ],
+      });
     });
   });
 });
